68a836c5746145c646dad42908b17c417f77f618
younjaewon <97930034+younjaewon@users.noreply.github.com>
2022-03-27 20:38:47 +0900

    first commit

---
+++ src/containers/CounterContainer.js
@@ -0,0 +1,23 @@
+import Counter from '../components/Counter';
+import { connect } from "react-redux";
+import { decreaseAsync, increaseAsync } from '../modules/counter';
+
+const CounterContainer = ({ number, increaseAsync, decreaseAsync }) => {
+  return (
+    <Counter
+      number={number}
+      onIncrease={increaseAsync}
+      onDecrease={decreaseAsync}
+    />
+  );
+};
+
+export default connect(
+  (state) => ({
+    number: state.counter,
+  }),
+  {
+    increaseAsync,
+    decreaseAsync,
+  }
+)(CounterContainer);
--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { applyMiddleware, createStore } from 'redux';
+import rootReducer from './modules';
+import { Provider } from 'react-redux';
+// import loggerMiddleware from './lib/loggerMiddleware';
+import { createLogger } from 'redux-logger';
+import ReduxThunk from 'redux-thunk'
+
+const logger = createLogger()
+const store = createStore(rootReducer, applyMiddleware(logger,ReduxThunk));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
