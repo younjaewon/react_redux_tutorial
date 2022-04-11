3f47adba261cefbcfd382e99b88521e22e62a4e8
younjaewon <97930034+younjaewon@users.noreply.github.com>
2022-04-11 21:35:51 +0900

    sample reducer add

--- src/modules/sample.js
+++ src/modules/sample.js
@@ -1,5 +1,44 @@
 import * as api from '../lib/api';
 
-// 액션 타입을 선언합ㄴ디ㅏ.
+// 액션 타입을 선언합니다.
 // 한 요청당 세 개를 만들어야 합니다.
+
+const GET_POST = 'sample/GET_POST';
+const GET_POST_SUCCESS = 'sample/GET_POST_SUCCESS';
+const GET_POST_FAILURE = 'sample/GET_POST_FAILURE';
+
+const GET_USERS = 'sample/GET_USERS';
+const GET_USERS_SUCCESS = 'sample/GET_USERS_SUCCESS';
+const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
+
+//think 함수를 생성합니다.
+// thunk 함수 내부에서는 시작할 때, 성공했을 때, 실패했을 때 다른 액션을 디스패치 합니다.
+
+export const getPost = id => async dispatch => {
+  dispatch({ type: GET_POST }); // 요청을 시작한 것을 알림
+  try {
+    const response = await api.getPost(id);
+    dispatch({
+      type: GET_POST_SUCCESS,
+      payload: response.data
+    }) // 요청 성공
+  } catch (e) {
+    dispatch({
+      type: GET_POST_FAILURE,
+      payload: e,
+      error: true,
+    }); // 에러 발생
+    throw e // 나중에 컴포넌트단에서 에러를 조회할 수 있게 해 줌
+  }
+}
+
+export const getUsers = () => async dispatch => {
+  dispatch({ type: GET_USERS }); //요청을 시작한 것을 알림
+  try {
+    
+  } catch (e) {
+    
+  }
+}
+
 
