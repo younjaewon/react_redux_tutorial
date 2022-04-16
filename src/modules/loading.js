05d7c446c6a8416df3e21f460d4eacacdbf5348f
younjaewon <97930034+younjaewon@users.noreply.github.com>
2022-04-16 22:52:29 +0900

    loading reducer add

--- src/modules/loading.js
+++ src/modules/loading.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import { createAction, handleActions } from 'redux-actions';
 
-const loading = () => {
-  return (
-    <div>
-      
-    </div>
-  );
-};
+const START_LOADING = 'loading/START_LOADING';
+const FINISH_LOADING = 'loading/FINISH_LOADING';
+
+// 요청을 위한 액션 타입을 payload로 설정합니다(예: "smaple/GET_POST").
+
+export const startLoading = createAction(START_LOADING,requestType => requestType);
+export const finishLoading = createAction(FINISH_LOADING, rquestType => rquestType);
+
+const initialState = {};
+
+const loading = handleActions(
+  {
+    [START_LOADING]: (state, action) => ({
+      ...state,
+      [action.payload]: true
+    }),
+    [FINISH_LOADING]: (state, action) => ({
+      ...state,
+      [action.payload]: false
+    })
+  },
+  initialState
+);
 
 export default loading;
